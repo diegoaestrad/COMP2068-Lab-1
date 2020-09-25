858067dea8449d1ce5714d10d4c0e06edc6d5794
Diego Estrada Beltran <86diegoestrada@gmail.com>
2020-09-25 17:00:01 +0000

    fix code add the ; after add a few lines and print the code Diego Estrada

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
 console.log('Hello world');
-console.log('Hello after edit in Github Platform')
+
+//The following line was edited from the browser without the ; 
+console.log('Hello after edit in Github Platform');
+const studentName = "Diego Alejandro Estrada Beltran";
+const studentNumber = "200427046";
+console.log("\n"+studentName + "\n" + studentNumber);
+    
 
